a00b267366417aee5af35e020e9a12c0fc4529ee
Henrique de Campos Duller <henriqueduller@hotmail.com>
2022-04-22 18:37:33 -0300

    loaded map insed an char **

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 16:48:44 by hde-camp          #+#    #+#             */
-/*   Updated: 2022/04/22 17:24:58 by hde-camp         ###   ########.fr       */
+/*   Updated: 2022/04/22 18:33:01 by hde-camp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,11 @@
 
 int	main(int argc, char *argv[])
 {
-	char *line;
-	int	map_fd;
+	t_map	*map;
 
 	(void)argc;
 	(void)argv;
-	map_fd = open("./maps/test.cub", O_RDONLY);
-	if (map_fd != -1)
-	{
-		line = get_next_line(map_fd);
-		while(line)
-		{
-			printf("%s",line);
-			free(line);
-			line = get_next_line(map_fd);
-		}
-	}
+	map = load_map("./maps/test.cub");
+	(void)map;
 	return (0);
 }
--- src/parser_load_map.c
+++ src/parser_load_map.c
@@ -6,11 +6,59 @@
 /*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 17:14:11 by hde-camp          #+#    #+#             */
-/*   Updated: 2022/04/22 17:42:44 by hde-camp         ###   ########.fr       */
+/*   Updated: 2022/04/22 18:35:20 by hde-camp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
+
+void	unload_map(t_map *map)
+{
+	int	i;
+
+	if (map)
+	{
+		if (map->lines)
+		{
+			i = 0;
+			while(map->lines[i])
+				free(map->lines[i++]);
+			free(map->lines);
+		}
+		free(map);
+	}
+}
+
+void	add_next_lines(char *new_line, char ***lines)
+{
+	int		cur_size;
+	int		new_size;
+	int		i;
+	char	**new_line_array;
+
+	if (!lines[0])
+	{
+		*lines = ft_calloc(2, sizeof(char *));
+		*lines[0] = new_line;
+	}
+	else
+	{
+		cur_size = 0;
+		while ((*lines)[cur_size])
+			cur_size++;
+		new_size = cur_size + 1;
+		new_line_array = ft_calloc(new_size + 1, sizeof(char *));
+		i = 0;
+		while (i < cur_size)
+		{
+			new_line_array[i] = (*lines)[i];
+			i++;
+		}
+		new_line_array[i] = new_line;
+		free((*lines));
+		(*lines) = new_line_array;
+	}
+}
 
 t_map	*load_map(const char *path)
 {
@@ -24,13 +72,12 @@ t_map	*load_map(const char *path)
 		printf("Arquivo %s nao encontrado.\n", path);
 		exit(EXIT_FAILURE);
 	}
-	map = malloc(sizeof(map));
-	
+	map = ft_calloc(1, sizeof(t_map));
 	line = get_next_line(fd);
 	while (line)
 	{
-
+		add_next_lines(line, &map->lines);
+		line = get_next_line(fd);
 	}
-	
-	return (map);
+	return (NULL);
 }

88d6fb232c983ef928ba77b701592731941f6a55
Denny Piza <dennypiza@hotmail.com>
2022-04-27 18:30:03 -0300

    fixed syntax evaluation for colors and refactor texture validation

--- src/parser_eval_assets.c
+++ src/parser_eval_assets.c
@@ -6,7 +6,7 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 21:49:33 by dpiza             #+#    #+#             */
-/*   Updated: 2022/04/25 18:14:20 by dpiza            ###   ########.fr       */
+/*   Updated: 2022/04/27 18:26:21 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,8 @@ static void	fill_texture_path(t_map *map, char *line)
 static int	is_valid_texture(char *line)
 {
 	int			i;
-	static char	*path[4] = {
-		"NO",
-		"SO",
-		"WE",
-		"EA"
-	};
+	static char	*path[4] = {"NO", "SO", "WE", "EA"};
 
-	if (!line)
-		return (0);
 	i = 0;
 	while (path[i] && ft_strncmp(line, path[i], 2) != 0)
 		i++;
@@ -49,35 +42,28 @@ static int	is_valid_texture(char *line)
 	line[ft_strlen(line) - 1] = '\0';
 	if (open(ft_strchr(line, '.'), O_RDONLY) > 0)
 		return (1);
-	else
-		perror("Load Asset");
+	return (0);
 }
 
 static void	fill_color(t_map *map, char *line)
 {
 	char	**color;
+	char	plane;
 	int		i;
 
+	plane = line[0];
+	line++;
+	while (ft_isspace(*line))
+		line++;
+	color = ft_split(line, ',');
 	i = 0;
-	if (line[0] == 'F')
+	while (color[i])
 	{
-		color = ft_split(line + 2, ',');
-		while (color[i])
-		{
-			printf("Atoi Color[i]: %i\n", ft_atoi(color[i]));
+		if (plane == 'F')
 			map->floor_c[i] = ft_atoi(color[i]);
-			i++;
-		}
-	}
-	else if (line[0] == 'C')
-	{
-		color = ft_split(line + 2, ',');
-		while (color[i])
-		{
-			printf("Atoi Color[i]: %i\n", ft_atoi(color[i]));
+		else
 			map->ceiling_c[i] = ft_atoi(color[i]);
-			i++;
-		}
+		i++;
 	}
 	ft_split_free(&color);
 }
@@ -87,30 +73,30 @@ static int	is_valid_color(char *line)
 	char	**color;
 	int		i;
 
-	if (ft_strncmp(line, "F", 1) == 0
-		|| ft_strncmp(line, "C", 1) == 0)
-	{
-		i = 0;
-		line[ft_strlen(line) - 1] = '\0';
-		color = ft_split(line + 2, ',');
-		while (color[i] && ft_atoi(color[i]) >= 0 && ft_atoi(color[i]) < 256)
-		{
-			printf("Color[i]: %s\n", color[i]);
-			i++;
-		}
-		ft_split_free(&color);
-		if (i > 2)
-			return (1);
-	}
+	if (ft_strncmp(line, "F", 1) && ft_strncmp(line, "C", 1))
+		return (0);
+	line++;
+	while (ft_isspace(*line))
+		line++;
+	if (!ft_isdigit(*line))
+		return (0);
+	line[ft_strlen(line) - 1] = '\0';
+	color = ft_split(line, ',');
+	i = 0;
+	while (color[i] && ft_atoi(color[i]) >= 0 && ft_atoi(color[i]) < 256)
+		i++;
+	ft_split_free(&color);
+	if (i > 2)
+		return (1);
 	return (0);
 }
 
 void	print_struct(t_map *map)
 {
 	printf("no_path: %s\nso_path: %s\nwe_path: %s\nea_path: %s\nfloor_c[0]: %d\nfloor_c[1]: %d\nfloor_c[2]: %d\nceiling_c[0]: %d\nceiling_c[1]: %d\nceiling_c[2]: %d\n",
-	map->no_path, map->so_path, map->we_path, map->ea_path,
-	map->floor_c[0], map->floor_c[1], map->floor_c[2],
-	map->ceiling_c[0], map->ceiling_c[1], map->ceiling_c[2]);
+		map->no_path, map->so_path, map->we_path, map->ea_path,
+		map->floor_c[0], map->floor_c[1], map->floor_c[2],
+		map->ceiling_c[0], map->ceiling_c[1], map->ceiling_c[2]);
 }
 
 void	free_struct(t_map *map)

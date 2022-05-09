c24310b37d54488033cf81ba4d9ed788cdf40774
Henrique de Campos Duller <henriqueduller@hotmail.com>
2022-05-09 19:42:00 -0300

    added new hooks. + comments on other input codes

--- src/hooks.c
+++ src/hooks.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   hooks.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
+/*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 16:01:01 by dpiza             #+#    #+#             */
-/*   Updated: 2022/05/06 17:18:45 by dpiza            ###   ########.fr       */
+/*   Updated: 2022/05/09 19:30:37 by hde-camp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,24 @@ void	winclose(t_mlx *mlx)
 
 int	key_hook(int k, t_mlx *mlx)
 {
-	if (k == 65307) // ESC
+	if (k == 0xff1b) // ESC
 		winclose(mlx);
+	if (k == 0x0061)
+		printf("a pressed\n");
+	/*
+	teclas de interesse:
+				normal		UPPERCASE
+	a			0x0061		0x0041
+	s			0x0073		0x0053
+	d			0x0064		0x0044
+	w			0x0077		0x0057
+	p			0x0070		0x0050
+	escape		0xff1b
+	enter		0xff0d
+	uparrow		0xff52
+	downarrow	0xff54
+	leftarrow	0xff51
+	rightarrow	0xff53
+	*/
 	return (0);
 }

3166467beec3f9ba27cecc7f8d3a1db43da0a3cb
Henrique de Campos Duller <henriqueduller@hotmail.com>
2022-05-09 22:45:15 -0300

    added some handlers

--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 16:01:01 by dpiza             #+#    #+#             */
-/*   Updated: 2022/05/09 19:30:37 by hde-camp         ###   ########.fr       */
+/*   Updated: 2022/05/09 22:45:00 by hde-camp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,18 @@ int	key_hook(int k, t_mlx *mlx)
 {
 	if (k == 0xff1b) // ESC
 		winclose(mlx);
-	if (k == 0x0061)
+	if (k == 0x0061 || k == 0x0041)
 		printf("a pressed\n");
+	if (k == 0x0073 || k == 0x0053)
+		printf("s pressed\n");
+	if (k == 0x0064 || k == 0x0044)
+		printf("d pressed\n");
+	if (k == 0x0077 || k == 0x0057)
+		printf("w pressed\n");
+	if (k == 0x0070 || k == 0x0050)
+		printf("p pressed\n");
+	if (k == 0xff0d)
+		printf("return pressed\n");
 	/*
 	teclas de interesse:
 				normal		UPPERCASE

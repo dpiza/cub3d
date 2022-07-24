e5521afcbe084b6ebd9749ef88a8b9d1ee84434a
Denny Piza <dennypiza@hotmail.com>
2022-07-24 09:30:20 -0300

    moved override_imgs_bg to the proper file

--- src/override_images.c
+++ src/override_images.c
@@ -6,7 +6,7 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 18:42:55 by hde-camp          #+#    #+#             */
-/*   Updated: 2022/07/23 23:02:35 by dpiza            ###   ########.fr       */
+/*   Updated: 2022/07/24 09:28:45 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,35 @@ void	override_images(t_mlx_img *dst, t_mlx_img *src, int x, int y)
 			i_x[1] = i_x[0] + x;
 			i_y[1] = i_y[0] + y;
 			if (get_byte_offset(src, i_x[1], i_y[1]) < get_byte_offset(src, dst->width, dst->height))
+			{
+				*(unsigned int *)(dst_pix + get_byte_offset(dst, i_x[1], i_y[1])) = \
+				*(unsigned int *)(src_pix + get_byte_offset(src, i_x[0], i_y[0]));
+			}
+			i_x[0]++;
+		}
+		i_y[0]++;
+	}
+}
+
+void	override_images_bg(t_mlx_img *dst, t_mlx_img *src, int x, int y, unsigned int bg)
+{
+	char	*dst_pix;
+	char	*src_pix;
+	int		i_x[2];
+	int		i_y[2];
+
+	dst_pix = dst->data;
+	src_pix = src->data;
+	i_y[0] = 0;
+	while (i_y[0] < src->height)
+	{
+		i_x[0] = 0;
+		while (i_x[0] < src->width)
+		{
+			i_x[1] = i_x[0] + x;
+			i_y[1] = i_y[0] + y;
+			if (get_byte_offset(src, i_x[1], i_y[1]) < get_byte_offset(src, dst->width, dst->height)
+			&& *(unsigned int *)(src_pix + get_byte_offset(src, i_x[0], i_y[0])) != bg)
 			{
 				*(unsigned int *)(dst_pix + get_byte_offset(dst, i_x[1], i_y[1])) = \
 				*(unsigned int *)(src_pix + get_byte_offset(src, i_x[0], i_y[0]));
--- src/print_weapon.c
+++ src/print_weapon.c
@@ -6,40 +6,11 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 00:16:59 by dpiza             #+#    #+#             */
-/*   Updated: 2022/07/24 00:24:17 by dpiza            ###   ########.fr       */
+/*   Updated: 2022/07/24 09:28:31 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
-
-void	override_images_bg(t_mlx_img *dst, t_mlx_img *src, int x, int y, unsigned int bg)
-{
-	char	*dst_pix;
-	char	*src_pix;
-	int		i_x[2];
-	int		i_y[2];
-
-	dst_pix = dst->data;
-	src_pix = src->data;
-	i_y[0] = 0;
-	while (i_y[0] < src->height)
-	{
-		i_x[0] = 0;
-		while (i_x[0] < src->width)
-		{
-			i_x[1] = i_x[0] + x;
-			i_y[1] = i_y[0] + y;
-			if (get_byte_offset(src, i_x[1], i_y[1]) < get_byte_offset(src, dst->width, dst->height)
-			&& *(unsigned int *)(src_pix + get_byte_offset(src, i_x[0], i_y[0])) != bg)
-			{
-				*(unsigned int *)(dst_pix + get_byte_offset(dst, i_x[1], i_y[1])) = \
-				*(unsigned int *)(src_pix + get_byte_offset(src, i_x[0], i_y[0]));
-			}
-			i_x[0]++;
-		}
-		i_y[0]++;
-	}
-}
 
 void	print_weapon(t_cub3d *game)
 {
@@ -55,5 +26,5 @@ void	print_weapon(t_cub3d *game)
 	img->data = mlx_get_data_addr(img->img_ptr, &(img->bpp), &(img->line_size), &(img->endian));
 	img->width = width;
 	img->height = height;
-	override_images_bg(game->mlx->img, img, w_width / 2, w_height - 126 - height, 15404543);
+	override_images_bg(game->mlx->img, img, w_width / 2, w_height - 126 - height, (unsigned int)0x00EB0DFF);
 }

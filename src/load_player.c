035bcdb80a6a31216278437a2199971edc8de33e
Henrique de Campos Duller <henriqueduller@hotmail.com>
2022-07-27 18:40:22 -0300

    load_player.c is norm compliant

--- src/load_player.c
+++ src/load_player.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   load_player.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
+/*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 23:39:07 by hde-camp          #+#    #+#             */
-/*   Updated: 2022/07/23 16:11:00 by dpiza            ###   ########.fr       */
+/*   Updated: 2022/07/27 18:39:45 by hde-camp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,38 +17,34 @@ static void	build_player_rays(t_player *player);
 
 void	load_player(t_cub3d	*game, t_player	*player)
 {
-	int			y_max;
-	int			x_max;
 	int			count;
 	char		*map_v;
 	t_strmap	*map;
 
 	map = game->map;
-	y_max = map->lines;
-	x_max = map->columns;
 	player->fov = FOV;
 	count = 0;
 	player->game = game;
 	player->rays = ft_calloc(player->n_rays + 1, sizeof(t_point));
 	player->collisions = ft_calloc(player->n_rays + 1, sizeof(t_collision));
-	while (count < y_max * x_max)
+	while (count < map->lines * map->columns)
 	{
 		map_v = ft_strchr("NSEW", (int)map->map[count]);
 		if (map_v)
 		{
-			player->pos.x = (count % x_max) + 0.5;
-			player->pos.y = (count / x_max) + 0.5;
+			player->pos.x = (count % map->columns) + 0.5;
+			player->pos.y = (count / map->columns) + 0.5;
 			set_player_dir(player, (char)map->map[count]);
 			set_fov_vectors(game);
 			build_player_rays(&game->player);
 			set_collisions(game);
-			break;
+			break ;
 		}
 		count++;
 	}
 }
 
-void	build_player_rays(t_player *player) //new
+void	build_player_rays(t_player *player)
 {
 	t_point	dst;
 	t_point	increment;
@@ -58,7 +54,7 @@ void	build_player_rays(t_player *player) //new
 
 	fov_fraction = ft_calloc(player->n_rays, sizeof(t_point));
 	increment = player->fov_vec[1];
-	multiply_vector_by_n((1/((float)player->n_rays - 1)*2),&increment);
+	multiply_vector_by_n((1 / ((float)player->n_rays - 1) * 2), &increment);
 	count = 0;
 	while (count < player->n_rays)
 	{
@@ -73,37 +69,6 @@ void	build_player_rays(t_player *player) //new
 	free(fov_fraction);
 }
 
-// void	build_player_rays(t_player *player) //old
-// {
-// 	t_point	dst;
-// 	t_point	*fov_fraction;
-// 	float	frac_num[3]; // [0]/[1] = [2]
-// 	int		count;
-
-// 	frac_num[1] = (float)(player->n_rays / 2);
-// 	fov_fraction = ft_calloc(player->n_rays + 1, sizeof(t_point));
-// 	count = player->n_rays / 2 - 1;
-// 	while (count > -1)
-// 	{
-// 		frac_num[0] = (float)count + 1;
-// 		frac_num[2] = frac_num[0] / frac_num[1];
-// 		fov_fraction[count] = player->fov_vec[0];
-// 		multiply_vector_by_n(frac_num[2], &fov_fraction[count]);
-// 		fov_fraction[ (player->n_rays - 1) - count] = player->fov_vec[1];
-// 		multiply_vector_by_n(frac_num[2], &fov_fraction[(player->n_rays - 1) - count]);
-// 		count--;
-// 	}
-// 	count = 0;
-// 	while (count < player->n_rays)
-// 	{
-// 		dst = sum_vectors(&player->dir, &fov_fraction[count]);
-// 		dst = normalize_vector(dst);
-// 		player->rays[count] = dst;
-// 		count++;
-// 	}
-// 	free(fov_fraction);
-// }
-
 void	set_fov_vectors(t_cub3d *game)
 {
 	t_player	*player;
@@ -114,8 +79,8 @@ void	set_fov_vectors(t_cub3d *game)
 	player->fov_vec[1] = player->dir;
 	multiply_vector_by_n((float)player->fov / 90, &player->fov_vec[0]);
 	multiply_vector_by_n((float)player->fov / 90, &player->fov_vec[1]);
-	sin_cos[0] = sinf(- M_PI /2);
-	sin_cos[1] = cosf(- M_PI /2);
+	sin_cos[0] = sinf(-M_PI / 2);
+	sin_cos[1] = cosf(-M_PI / 2);
 	rotate_vector_new(sin_cos[0], sin_cos[1], &player->fov_vec[0]);
 	sin_cos[0] = sinf(M_PI / 2);
 	sin_cos[1] = cosf(M_PI / 2);

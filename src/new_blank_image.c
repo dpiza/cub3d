2a5e3a7c4d0745bba93bda2c4fa6315eb1752585
Henrique de Campos Duller <henriqueduller@hotmail.com>
2022-07-27 18:51:54 -0300

    load_game.c, new_blank_image.c norm compliant

--- src/new_blank_image.c
+++ src/new_blank_image.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   new_blank_image.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
+/*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 00:08:00 by hde-camp          #+#    #+#             */
-/*   Updated: 2022/07/23 16:31:24 by dpiza            ###   ########.fr       */
+/*   Updated: 2022/07/27 18:48:40 by hde-camp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,15 @@
 t_mlx_img	*new_blank_img(t_mlx *mlx, int width, int height)
 {
 	t_mlx_img	*img;
+	int			*bpp;
+	int			*l_size;
 
 	img = ft_calloc(1, sizeof(t_mlx_img));
 	img->mlx = mlx;
 	img->img_ptr = mlx_new_image(mlx->mlx_ptr, width, height);
-	img->data = mlx_get_data_addr(img->img_ptr, &(img->bpp), &(img->line_size), &(img->endian));
+	bpp = &(img->bpp);
+	l_size = &(img->line_size);
+	img->data = mlx_get_data_addr(img->img_ptr, bpp, l_size, &(img->endian));
 	img->width = width;
 	img->height = height;
 	return (img);

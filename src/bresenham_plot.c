8def7dc8dea46a2f0fab7a1c212b4e7d6e0dd53c
Henrique de Campos Duller <henriqueduller@hotmail.com>
2022-07-27 17:47:33 -0300

    bressenham_plot.c norm compliant

--- src/bresenham_plot.c
+++ src/bresenham_plot.c
@@ -6,7 +6,7 @@
 /*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 20:26:00 by hde-camp          #+#    #+#             */
-/*   Updated: 2022/07/27 17:07:15 by hde-camp         ###   ########.fr       */
+/*   Updated: 2022/07/27 17:47:07 by hde-camp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,34 @@ void	put_image_pixel(t_mlx_img *img, int x, int y, unsigned int color)
 	}
 }
 
-void	bresenham_line(t_mlx_img *img, t_int_point src, t_int_point dst, unsigned int color)
+void	start_variables(t_int_point	*sxy, t_int_point	*delta, \
+	t_int_point *src, t_int_point *dst)
 {
-	t_int_point s;
-	t_int_point delta;
-	int err;
-	int e2;
+	delta->x = abs(dst->x - src->x);
+	delta->y = -abs(dst->y - src->y);
+	sxy->x = -1;
+	sxy->y = -1;
+	if (src->x < dst->x)
+		sxy->x = 1;
+	if (src->y < dst->y)
+		sxy->y = 1;
+}
 
-	delta.x = abs(dst.x - src.x);
-	s.x = src.x < dst.x ? 1 : -1;
-	delta.y = -abs(dst.y - src.y);
-	s.y = src.y < dst.y ? 1 : -1;
+void	bresenham_line(t_mlx_img *img, t_int_point src, \
+	t_int_point dst, unsigned int color)
+{
+	t_int_point	s;
+	t_int_point	delta;
+	int			err;
+	int			e2;
+
+	start_variables(&s, &delta, &src, &dst);
 	err = delta.x + delta.y;
 	while (1)
 	{
 		put_image_pixel(img, src.x, src.y, color);
 		if (src.x == dst.x && src.y == dst.y)
-			break;
+			break ;
 		e2 = 2 * err;
 		if (e2 >= delta.y)
 		{

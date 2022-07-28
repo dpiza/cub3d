86b1c2b39bef3329cabe960dab4947d1badcbd72
Denny Piza <dennypiza@hotmail.com>
2022-07-28 10:28:18 -0300

    src/square_check.c norm compliant

--- src/square_check.c
+++ src/square_check.c
@@ -6,7 +6,7 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 16:15:20 by dpiza             #+#    #+#             */
-/*   Updated: 2022/07/23 16:15:33 by dpiza            ###   ########.fr       */
+/*   Updated: 2022/07/28 10:28:07 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,8 @@ char	square_check(t_cub3d *game, float x, float y)
 {
 	int		pos_array;
 	char	map_obj;
-	
+
 	pos_array = (int)game->map->columns * (int)y + (int)x;
 	map_obj = game->map->map[pos_array];
-	// printf("Map obj %c\n", map_obj); // printa o objeto do mapa no terminal
 	return (map_obj);
 }

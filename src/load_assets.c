0a5e81f84a437eb909f0c37ac3e582563edadace
Denny Piza <dennypiza@hotmail.com>
2022-07-28 10:26:05 -0300

    load_assets.c norm compliant

--- src/load_assets.c
+++ src/load_assets.c
@@ -6,7 +6,7 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 11:30:04 by dpiza             #+#    #+#             */
-/*   Updated: 2022/07/26 21:17:15 by dpiza            ###   ########.fr       */
+/*   Updated: 2022/07/28 10:25:03 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ t_mlx_img	*load_texture(t_cub3d *game, char *filename)
 
 	img = ft_calloc(1, sizeof(t_mlx_img));
 	img->mlx = game->mlx;
-	img->img_ptr = mlx_xpm_file_to_image(game->mlx->mlx_ptr, filename, &img->width, &img->height);
-	img->data = mlx_get_data_addr(img->img_ptr, &(img->bpp), &(img->line_size), &(img->endian));
+	img->img_ptr = mlx_xpm_file_to_image(game->mlx->mlx_ptr, \
+		filename, &img->width, &img->height);
+	img->data = mlx_get_data_addr(img->img_ptr, &(img->bpp), \
+		&(img->line_size), &(img->endian));
 	return (img);
 }
 
@@ -27,19 +29,18 @@ void	load_assets(t_cub3d *game)
 {
 	unsigned char	*floor;
 	unsigned char	*ceiling;
-	
+
 	floor = game->s_map->floor_c;
 	ceiling = game->s_map->ceilling_c;
 	game->texture_no = load_texture(game, game->s_map->no_path);
 	game->texture_so = load_texture(game, game->s_map->so_path);
 	game->texture_ea = load_texture(game, game->s_map->ea_path);
 	game->texture_we = load_texture(game, game->s_map->we_path);
-	game->bar = load_texture(game, "./textures/bar/fo_bar_nuk3d_darker_weapon.xpm");
-	game->bar_screen = load_texture(game, "./textures/bar/fo_bar_screen_painted.xpm");
-	game->weapon = load_texture(game, "./textures/weapon/weapon_01.xpm");
-	game->weapon_fire = load_texture(game, "./textures/weapon/weapon_01_firing.xpm");
+	game->bar = load_texture(game, "./textures/bar/bar.xpm");
+	game->bar_screen = load_texture(game, "./textures/bar/bar_screen.xpm");
+	game->weapon = load_texture(game, "./textures/weapon/weapon.xpm");
+	game->weapon_fire = load_texture(game, "./textures/weapon/weapon_f.xpm");
 	game->crosshair = load_texture(game, "./textures/crosshair.xpm");
 	game->floor = floor[0] << 16 | floor[1] << 8 | floor[2];
 	game->ceilling = ceiling[0] << 16 | ceiling[1] << 8 | ceiling[2];
-	// printf("Floor: %i\nCeilling: %i\n", game->floor, game->ceilling);
 }

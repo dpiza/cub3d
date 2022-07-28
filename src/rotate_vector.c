658e592a00d8206c2080cac53146ef748c572b05
Denny Piza <dennypiza@hotmail.com>
2022-07-28 09:32:49 -0300

    rotate_vector.c norm

--- src/rotate_vector.c
+++ src/rotate_vector.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   rotate_vector.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hde-camp <hde-camp@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 14:33:20 by hde-camp          #+#    #+#             */
-/*   Updated: 2022/07/12 21:36:28 by hde-camp         ###   ########.fr       */
+/*   Updated: 2022/07/28 09:32:32 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-void	rotate_vector_old(float	angle, t_point *vector)
+void	rotate_vector_old(float angle, t_point *vector)
 {
 	t_point	x_y;
 	float	cos;
@@ -21,10 +21,10 @@ void	rotate_vector_old(float	angle, t_point *vector)
 	x_y.x = (*vector).x;
 	x_y.y = (*vector).y;
 	cos = cosf(angle);
-	if (cos*cos < 0.00000001) //this ensures cos >= 0.0001 is rounded to zero
+	if (cos * cos < 0.00000001)
 		cos = 0;
 	sin = sinf(angle);
-	if (sin*sin < 0.00000001)
+	if (sin * sin < 0.00000001)
 		sin = 0;
 	(*vector).x = x_y.x * cos - x_y.y * sin;
 	(*vector).y = x_y.x * sin + x_y.y * cos;
